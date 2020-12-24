742984df72122e7dbd887ebdb1625916a26de3b5
Andrew Caniglia <andrewcaniglia@gmail.com>
2020-12-24 03:30:43 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -4,17 +4,17 @@ CREATE table projects (
   funding_goal TEXT,
   start_date INTEGER,
   end_date INTEGER
-)
+);
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
-)
+);
 
 CREATE TABLE pledge (
   id INTEGER PRIMARY KEY,
   amount INTEGER,
   user_id INTEGER,
   project_id INTEGER
-)
+);
