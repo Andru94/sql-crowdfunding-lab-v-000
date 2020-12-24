cc70d2b63ce250a36db3d049672470d854171bc9
Andrew Caniglia <andrewcaniglia@gmail.com>
2020-12-24 05:02:18 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,6 +31,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   ON projects.id = pledges.project_id
   HAVING SUM(pledges.amount) > projects.funding_goal
   GROUP BY projects.title
+  HAVING SUM(pledges.amount) > projects.funding_goal
   "
 end
 
