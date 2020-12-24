9b16b7ec2ea7bf23d782f780c47032d4ef063dd9
Andrew Caniglia <andrewcaniglia@gmail.com>
2020-12-24 04:56:52 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,11 +25,12 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.titles, projects.amount
+  "SELECT projects.title, projects.amount
   FROM projects
   INNER JOIN pledges
   ON projects.id = pledges.project_id
-  HAVING
+  GROUP BY projects.title
+  HAVING SUM(pledges) > projects.amount
   "
 end
 
